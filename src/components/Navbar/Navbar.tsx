9c6e8944ae3118e25c7b46c0521b3f0b0c32f08a
thanhphucnguyen1810 <thanhphucnguyen54@gmail.com>
2025-06-29 17:42:55 +0700

    feat: Topbar Update Auth

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,14 +1,13 @@
 import { authModalState } from '~/atoms/authModalAtom';
+import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
 import { useSetRecoilState } from 'recoil';
 
 
-type NavbarProps = {
-  
-};
+type NavbarProps = {};
 
-const Navbar:React.FC<NavbarProps> = () => {
+const Navbar: React.FC<NavbarProps> = () => {
   const setAuthModalState = useSetRecoilState(authModalState)
   const handleClick = () => {
     setAuthModalState((prev) => ({ ...prev, isOpen: true}));
@@ -16,7 +15,7 @@ const Navbar:React.FC<NavbarProps> = () => {
   
   return <div className="flex items-center justify-between sm:px-12 px-2 md:px-24">
     <Link href="/" className='flex items-center justify-center h-20'>
-      <img src="/logo.png" alt="LeetClone" className="h-full"/>
+      <Image src='/logo.png' alt='LeetCode' height={200} width={200} />
     </Link>
 
     <div className="flex items-center">
---
+++ src/pages/problems/[pid].tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Topbar from '~/components/Topbar/Topbar'
+
+type ProblemPageProps = {}
+
+const ProblemPage: React.FC<ProblemPageProps> = ({  }) => {
+  return (
+    <div>
+      <Topbar problemPage />
+    </div>
+  )
+}
+
+export default ProblemPage
+
